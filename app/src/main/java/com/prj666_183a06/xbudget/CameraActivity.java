83b9c4082457eb4bec63a12d90a961bce59cee56
Adam <adam.kolodko@hotmail.com>
2018-10-13 15:50:22 -0400

    Added Activity and 2 supporing classes for OCR

---
+++ app/src/main/java/com/prj666_183a06/xbudget/CameraActivity.java
@@ -0,0 +1,13 @@
+package com.prj666_183a06.xbudget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cameraactivity);
+    }
+}
