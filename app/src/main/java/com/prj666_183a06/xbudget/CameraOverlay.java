67da316d7efabeeaac6695386cb36f45c101ae6e
Adam <adam.kolodko@hotmail.com>
2018-10-13 16:07:59 -0400

    Added Required functions and supporting camera classes.

--- app/src/main/java/com/prj666_183a06/xbudget/CameraOverlay.java
+++ app/src/main/java/com/prj666_183a06/xbudget/CameraOverlay.java
@@ -1,4 +1,23 @@
 package com.prj666_183a06.xbudget;
 
+import android.graphics.Canvas;
+
+import com.prj666_183a06.xbudget.camera.GraphicOverlay;
+
+import java.util.List;
+
 public class CameraOverlay extends GraphicOverlay.Graphic {
+    public CameraOverlay(GraphicOverlay overlay) {
+        super(overlay);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    public boolean contains(float x, float y) {
+        return false;
+    }
 }
--- app/src/main/java/com/prj666_183a06/xbudget/OCRParser.java
+++ app/src/main/java/com/prj666_183a06/xbudget/OCRParser.java
@@ -1,4 +1,18 @@
 package com.prj666_183a06.xbudget;
 
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+
+import java.lang.Object;
+
 public class OCRParser implements Detector.Processor<TextBlock> {
+    @Override
+    public void release() {
+
+    }
+
+    @Override
+    public void receiveDetections(Detector.Detections<TextBlock> detections) {
+
+    }
 }
