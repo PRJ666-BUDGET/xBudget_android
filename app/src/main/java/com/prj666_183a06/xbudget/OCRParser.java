c7b73c6448ce2bbac7975977903046a4c3a20212
Adam <adam.kolodko@hotmail.com>
2018-10-14 14:00:34 -0400

    Ported OCR Parser

--- app/src/main/java/com/prj666_183a06/xbudget/OCRParser.java
+++ app/src/main/java/com/prj666_183a06/xbudget/OCRParser.java
@@ -1,18 +1,50 @@
 package com.prj666_183a06.xbudget;
 
+import android.util.Log;
+import android.util.SparseArray;
+
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.text.TextBlock;
+import com.prj666_183a06.xbudget.camera.GraphicOverlay;
 
-import java.lang.Object;
-
+/**
+ * A very simple Processor which gets detected TextBlocks and adds them to the overlay
+ * as OcrGraphics.
+ */
 public class OCRParser implements Detector.Processor<TextBlock> {
-    @Override
-    public void release() {
 
+    private GraphicOverlay<CameraOverlay> graphicOverlay;
+
+    OCRParser(GraphicOverlay<CameraOverlay> ocrGraphicOverlay) {
+        graphicOverlay = ocrGraphicOverlay;
     }
 
+    /**
+     * Called by the detector to deliver detection results.
+     * If your application called for it, this could be a place to check for
+     * equivalent detections by tracking TextBlocks that are similar in location and content from
+     * previous frames, or reduce noise by eliminating TextBlocks that have not persisted through
+     * multiple detections.
+     */
     @Override
     public void receiveDetections(Detector.Detections<TextBlock> detections) {
+        graphicOverlay.clear();
+        SparseArray<TextBlock> items = detections.getDetectedItems();
+        for (int i = 0; i < items.size(); ++i) {
+            TextBlock item = items.valueAt(i);
+            if (item != null && item.getValue() != null) {
+                Log.d("OcrDetectorProcessor", "Text detected! " + item.getValue());
+                CameraOverlay graphic = new CameraOverlay(graphicOverlay, item);
+                graphicOverlay.add(graphic);
+            }
+        }
+    }
 
+    /**
+     * Frees the resources associated with this detection processor.
+     */
+    @Override
+    public void release() {
+        graphicOverlay.clear();
     }
 }
