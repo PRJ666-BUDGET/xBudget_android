3256bb77a443c0775f347172e06e8f5601106a0d
Adam <adam.kolodko@hotmail.com>
2018-10-14 14:01:38 -0400

    Floating button opens Camera activity

--- app/src/main/java/com/prj666_183a06/xbudget/MainActivity.java
+++ app/src/main/java/com/prj666_183a06/xbudget/MainActivity.java
@@ -33,7 +33,9 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
-                Intent openCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                Intent openCamera = new Intent(MainActivity.this, CameraActivity.class);
+                startActivity(openCamera);
             }
         });
 
