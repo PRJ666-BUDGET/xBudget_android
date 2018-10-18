ab34d1ef568fdcaf481f0dd282ffe37e8199e707
Irene <jbae18@myseneca.ca>
2018-10-18 19:14:08 -0400

    Add bar graph showing rate of expenses

--- app/src/main/java/com/prj666_183a06/xbudget/HomeActivity.java
+++ app/src/main/java/com/prj666_183a06/xbudget/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.prj666_183a06.xbudget;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,12 +10,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
@@ -23,7 +33,14 @@ import java.util.ArrayList;
 public class HomeActivity extends Fragment {
 
     private LineChart mLine;
+    private HorizontalBarChart mBar;
     private Typeface tf;
+
+    private double mIncome;
+    private double mSpent;
+    private double mBalance;
+    private double mRate;
+    private TextView mtvIncome, mtvSpent, mtvBalance;
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
@@ -36,6 +53,21 @@ public class HomeActivity extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.activity_home, container, false);
+
+        // Get Data
+        mIncome = 2000.00;
+        mSpent = 1260.00;
+        mBalance = mIncome - mSpent;
+        mRate = mSpent / mIncome;
+
+        mtvIncome = (TextView) v.findViewById(R.id.tvIncome);
+        mtvIncome.setText(String.valueOf(mIncome));
+
+        mtvSpent = (TextView) v.findViewById(R.id.tvSpent);
+        mtvSpent.setText(String.valueOf(mSpent));
+
+        mtvBalance = (TextView) v.findViewById(R.id.tvBalance);
+        mtvBalance.setText(String.valueOf(mBalance));
 
         FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -58,49 +90,102 @@ public class HomeActivity extends Fragment {
 
         mLine.setData(generateLineData());
 
+        // Bar Chart
+        mBar = v.findViewById(R.id.barChart1);
+        mBar.getDescription().setEnabled(false);
+
+        mBar.setData(generateBarData());
+        mBar.setDrawGridBackground(false);
+
+        XAxis xAxis = mBar.getXAxis();
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawLabels(false);
+
+        YAxis rightAxis = mBar.getAxisRight();
+        rightAxis.setEnabled(false);
+        rightAxis.setDrawLabels(false);
+        rightAxis.setDrawGridLines(false);
+
+        YAxis leftAxis = mBar.getAxisLeft();
+        leftAxis.setTextColor(Color.GRAY);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setAxisMaximum(100f);
+//        leftAxis.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART);
+
+        Legend lgdBar = mBar.getLegend();
+        lgdBar.setTypeface(tf);
+        lgdBar.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+
+        mBar.setDrawValueAboveBar(false);
+        mBar.invalidate();
         return v;
     }
 
-    protected LineData generateLineData() {
+    protected BarData generateBarData() {
+        ArrayList<BarEntry> entries = new ArrayList<BarEntry>();
+//        for(int i = 0; i < 3; i++) {
+//            entries.add(new BarEntry(i, (float) ((Math.random() * 60) + 40), "Expenses " + (i+1)));
+//        }
 
-        ArrayList<Entry> budget = new ArrayList<Entry>();
-        ArrayList<Entry> expenses = new ArrayList<Entry>();
+        float v_expenses = 70;
+        float v_balance = 100 - v_expenses;
+
+        entries.add(new BarEntry(1, new float[] {v_expenses, v_balance}, ""));
+
+        BarDataSet ds = new BarDataSet(entries, "");
+        ds.setDrawIcons(false);
+        ds.setColors(ColorTemplate.VORDIPLOM_COLORS[4], ColorTemplate.VORDIPLOM_COLORS[3]);
+        ds.setStackLabels(new String[]{"Expenses", "Balance"});
+
+        ArrayList<IBarDataSet> iBarDataSets = new ArrayList<IBarDataSet>();
+        iBarDataSets.add(ds);
+
+        BarData barData = new BarData(ds);
+        barData.setValueFormatter(new MyValueFormatter());
+
+        return barData;
+    }
+
+    protected LineData generateLineData() {
+        ArrayList<Entry> budget_arrList = new ArrayList<Entry>();
+        ArrayList<Entry> expenses_arrList = new ArrayList<Entry>();
 
         LineDataSet budget_ds;
         LineDataSet expenses_ds;
 
-        ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+        ArrayList<ILineDataSet> iLineDataSets = new ArrayList<ILineDataSet>();
 
         // Budget Dataset
-        budget.add(new Entry(1, 2000));
-        budget.add(new Entry(8, 2000));
-        budget.add(new Entry(15, 2000));
-        budget.add(new Entry(22, 2000));
-        budget.add(new Entry(27, 2000));
+        budget_arrList.add(new Entry(1, 2000));
+        budget_arrList.add(new Entry(8, 2000));
+        budget_arrList.add(new Entry(15, 2000));
+        budget_arrList.add(new Entry(22, 2000));
+        budget_arrList.add(new Entry(27, 2000));
 
-        budget_ds = new LineDataSet(budget, "My Budget");
+        budget_ds = new LineDataSet(budget_arrList, "My Budget");
         budget_ds.setLineWidth(2f);
         budget_ds.setDrawCircles(false);
         budget_ds.setColor(ColorTemplate.VORDIPLOM_COLORS[0]);
 
-        dataSets.add(budget_ds);
+        iLineDataSets.add(budget_ds);
 
         // Expense Dataset
-        expenses.add(new Entry(1, 170));
-        expenses.add(new Entry(8, 220));
-        expenses.add(new Entry(15, 530));
-        expenses.add(new Entry(22, 700));
-        expenses.add(new Entry(27, 1070));
+        expenses_arrList.add(new Entry(1, 170));
+        expenses_arrList.add(new Entry(8, 220));
+        expenses_arrList.add(new Entry(15, 530));
+        expenses_arrList.add(new Entry(22, 700));
+        expenses_arrList.add(new Entry(27, 1070));
 
-        expenses_ds = new LineDataSet(expenses, "My Expenses");
+        expenses_ds = new LineDataSet(expenses_arrList, "My Expenses");
         expenses_ds.setLineWidth(2f);
         expenses_ds.setDrawCircles(false);
         expenses_ds.setColor(ColorTemplate.VORDIPLOM_COLORS[4]);
 
-        dataSets.add(expenses_ds);
+        iLineDataSets.add(expenses_ds);
 
-        LineData lineData = new LineData(dataSets);
-        lineData.setValueTypeface(tf);
+        LineData lineData = new LineData(iLineDataSets);
+//        lineData.setValueTypeface(tf);
         return lineData;
     }
 }
