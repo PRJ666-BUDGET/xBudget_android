8a4d184b1e6e4134536ab51aa6026130b0caf64b
Paolo Chang <chanhwan.chang@gmail.com>
2018-10-28 14:39:44 -0400

    Plan updates & delete are added (#24)
    * Plan updates & delete are added
    
    * plan view layout adjusted
    
    * Detail to list view go back button has fixed
    
    * Update goback button is fixed
    
    * Update cancel notice commented out

--- app/src/main/java/com/prj666_183a06/xbudget/crud/DetailPlanActivity.java
+++ app/src/main/java/com/prj666_183a06/xbudget/crud/DetailPlanActivity.java
@@ -1,9 +1,12 @@
 package com.prj666_183a06.xbudget.crud;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -11,10 +14,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.prj666_183a06.xbudget.R;
+import com.prj666_183a06.xbudget.adapter.PlanAdapter;
+import com.prj666_183a06.xbudget.database.entity.PlanEntity;
+import com.prj666_183a06.xbudget.viewmodel.PlanViewModel;
+
+import java.util.List;
 
 public class DetailPlanActivity extends AppCompatActivity {
 
     private static final String TAG = "DetailPlanActivity";
+
+    public static final int EDIT_PLAN_REQUEST = 3;
 
     public static final String PLAN_ID = "com.prj666_183a06.xbudget.PLAN_ID";
     public static final String PLAN_TYPE = "com.prj666_183a06.xbudget.PLAN_TYPE";
@@ -27,12 +37,26 @@ public class DetailPlanActivity extends AppCompatActivity {
     private TextView amountRecord;
     private TextView periodRecord;
 
+    private PlanViewModel planViewModel;
+
     private int id;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_plan);
+
+        final PlanAdapter adapter = new PlanAdapter();
+
+        planViewModel = ViewModelProviders.of(this).get(PlanViewModel.class);
+        planViewModel.getPlanList().observe(this, new Observer<List<PlanEntity>>() {
+            @Override
+            public void onChanged(@Nullable List<PlanEntity> plans) {
+                adapter.setPlans(plans);
+                // UPDATE RecyclerView
+                //Toast.makeText(getContext(),"onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         typeRecord = findViewById(R.id.textView_typeRecord);
         titleRecord = findViewById(R.id.textView_titleRecord);
@@ -41,13 +65,13 @@ public class DetailPlanActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
 
+        setId(intent.getIntExtra(PLAN_ID, -1));
         typeRecord.setText(intent.getStringExtra(PLAN_TYPE));
         titleRecord.setText(intent.getStringExtra(PLAN_TITLE));
         amountRecord.setText(String.valueOf(intent.getDoubleExtra(PLAN_AMOUNT, 0.00)));
         periodRecord.setText(intent.getStringExtra(PLAN_PERIOD));
 
-
-        Toast.makeText(this, "Plan is loaded.", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "PlanId " + getId() + " is loaded.", Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -79,7 +103,7 @@ public class DetailPlanActivity extends AppCompatActivity {
                 }
 
                 setResult(RESULT_OK, data);
-                Toast.makeText(this, "Plan is deleted", Toast.LENGTH_SHORT).show();
+                finish();
                 return true;
 
             case R.id.update_plan:
@@ -89,25 +113,67 @@ public class DetailPlanActivity extends AppCompatActivity {
                 int itemId = intent.getIntExtra(PLAN_ID, -1);
 
                 if (itemId == -1) {
-                    Toast.makeText(this, "Plan cannot be updated.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "BAD REQUEST [DetailPlanActivity.java onOptionsItemSelected()], itemId: " + itemId, Toast.LENGTH_SHORT).show();
                     return false;
                 }
 
                 Intent newIntent = new Intent(this, CreateUpdatePlanActivity.class);
 
-                newIntent.setFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT);
                 newIntent.putExtra(CreateUpdatePlanActivity.PLAN_ID, getId());
                 newIntent.putExtra(CreateUpdatePlanActivity.PLAN_TYPE, typeRecord.getText().toString());
                 newIntent.putExtra(CreateUpdatePlanActivity.PLAN_TITLE, titleRecord.getText().toString());
                 newIntent.putExtra(CreateUpdatePlanActivity.PLAN_AMOUNT, Double.parseDouble(amountRecord.getText().toString()));
                 newIntent.putExtra(CreateUpdatePlanActivity.PLAN_PERIOD, periodRecord.getText().toString());
-                this.finish();
-                startActivity(newIntent);
-                Toast.makeText(this, "UPDATE", Toast.LENGTH_SHORT).show();
+
+
+                startActivityForResult(newIntent, EDIT_PLAN_REQUEST);
+//                Toast.makeText(this, "UPDATE REQUEST [DetailPlanActivity.java onOptionsItemSelected()], itemId: " + itemId, Toast.LENGTH_SHORT).show();
                 return true;
 
             default:
-                return super.onOptionsItemSelected(item);
+//                Toast.makeText(this, "BACK BUTTON REQUEST [DetailPlanActivity.java onOptionsItemSelected()], itemId: " + getId(), Toast.LENGTH_SHORT).show();
+                if ( getFragmentManager().getBackStackEntryCount() > 0)
+                {
+                    getFragmentManager().popBackStack();
+                    return false;
+                }
+                super.onBackPressed();
+                return true;
+        }
+    }
+
+    /**
+     *  DATA RETRIEVES FROM CreateUpdatePlanActivity
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == EDIT_PLAN_REQUEST && resultCode == RESULT_OK) {
+
+            int id = data.getIntExtra(CreateUpdatePlanActivity.PLAN_ID, -1);
+
+            Log.d(TAG, "onActivityResult: planId: " + id);
+
+            if (id == -1) {
+                Toast.makeText(this, "BAD REQUEST [DetailPlanActivity.java: onActivityResult()], RETURN planId: " + id, Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            String type = data.getStringExtra(CreateUpdatePlanActivity.PLAN_TYPE);
+            String title = data.getStringExtra(CreateUpdatePlanActivity.PLAN_TITLE);
+            double amount = data.getDoubleExtra(CreateUpdatePlanActivity.PLAN_AMOUNT, 0.00);
+            String period = data.getStringExtra(CreateUpdatePlanActivity.PLAN_PERIOD);
+
+            PlanEntity plan = new PlanEntity(type ,title, amount, period);
+            plan.setPlanId(id);
+            planViewModel.update(plan);
+
+            Toast.makeText(this, title + " is updated.", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        else{
+//            Toast.makeText(this, "UPDATE CANCEL REQUEST [DetailPlanActivity.java: onActivityResult()].", Toast.LENGTH_SHORT).show();
         }
     }
 
