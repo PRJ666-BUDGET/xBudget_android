2aeb1355c4f08ba52df4120fc6271a56faa8a872
Paolo Chang <chanhwan.chang@gmail.com>
2018-10-29 00:29:06 -0400

    comment out Toast

--- app/src/main/java/com/prj666_183a06/xbudget/crud/DetailPlanActivity.java
+++ app/src/main/java/com/prj666_183a06/xbudget/crud/DetailPlanActivity.java
@@ -128,6 +128,7 @@ public class DetailPlanActivity extends AppCompatActivity {
 
                 startActivityForResult(newIntent, EDIT_PLAN_REQUEST);
 //                Toast.makeText(this, "UPDATE REQUEST [DetailPlanActivity.java onOptionsItemSelected()], itemId: " + itemId, Toast.LENGTH_SHORT).show();
+
                 return true;
 
             default:
