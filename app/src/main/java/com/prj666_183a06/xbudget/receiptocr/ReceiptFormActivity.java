3ec7233295ead867eb74d6214bb6d92e23050072
Adam <adam.kolodko@hotmail.com>
2018-11-09 13:15:32 -0500

    Create receiptFormActivity

---
+++ app/src/main/java/com/prj666_183a06/xbudget/receiptocr/ReceiptFormActivity.java
@@ -0,0 +1,31 @@
+package com.prj666_183a06.xbudget.receiptocr;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.prj666_183a06.xbudget.R;
+
+public class ReceiptFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receipt_form);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
