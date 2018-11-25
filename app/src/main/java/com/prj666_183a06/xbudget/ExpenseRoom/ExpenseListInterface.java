a8dec3a65f6c0096323a4562e814a8de19a6b359
jngu36 <jnguyen36@myseneca.ca>
2018-11-25 16:08:27 -0500

    Fixed costTotal function to 30 days

--- app/src/main/java/com/prj666_183a06/xbudget/ExpenseRoom/ExpenseListInterface.java
+++ app/src/main/java/com/prj666_183a06/xbudget/ExpenseRoom/ExpenseListInterface.java
@@ -1,11 +1,17 @@
 package com.prj666_183a06.xbudget.ExpenseRoom;
 
+import android.util.Log;
+
 import com.prj666_183a06.xbudget.viewmodel.PlanViewModel;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 public class ExpenseListInterface {
     private List<ExpenseObj> listE;
@@ -15,14 +21,14 @@ public class ExpenseListInterface {
     ExpenseViewModel vm;
     PlanViewModel pvm;
 
-    public ExpenseListInterface(ExpenseViewModel e){
+    public ExpenseListInterface(ExpenseViewModel e) {
         listE = new ArrayList<>();
         vm = e;
         listE = vm.getAll();
         titles = new ArrayList<>();
     }
 
-    public ExpenseListInterface(ExpenseViewModel e, PlanViewModel p){
+    public ExpenseListInterface(ExpenseViewModel e, PlanViewModel p) {
         listE = new ArrayList<>();
 
         vm = e;
@@ -31,30 +37,67 @@ public class ExpenseListInterface {
         titles = pvm.getTitleList();
     }
 
-    public String print(){
+    public String print() {
         String ret = "";
-        for(ExpenseObj r: listE){
+        for (ExpenseObj r : listE) {
             ret = ret + r.getItem() + " " + r.getStore() + " " + r.getDate() + " " + r.getCost() + "\n";
         }
         return ret;
     }
 
-    public double costTotal(){
+    public double costTotal() {
+
         double ret = 0;
-        for(ExpenseObj c: listE){
+        Calendar cal = Calendar.getInstance();
+
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH) + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        String date;
+
+        for (int i = 0; i < 30; i++) {
+
+            date = month + "/" + day  + "/" + year;
+
+            for (ExpenseObj c : listE) {
+                if(date.equals(c.getDate())){
+                    ret+= c.getCost();
+                }
+            }
+
+            if (day != 1) {
+                day--;
+            } else {
+                if (month != 1) {
+                    month--;
+                    day = 31;
+                } else {
+                    year--;
+                    month = 12;
+                    day = 31;
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    public double costTotalAll() {
+        double ret = 0;
+        for (ExpenseObj c : listE) {
             ret += c.getCost();
         }
         return ret;
     }
 
-    public List<Float> costTotalByCategory(){
-        List<Float> tempList= new ArrayList<>();
+    public List<Float> costTotalByCategory() {
+        List<Float> tempList = new ArrayList<>();
         float temp = 0;
-        if(!titles.isEmpty()) {
-            for(String r: titles){
-                for(ExpenseObj c: listE){
-                    if(r.equals(c.getCategory())){
-                        temp += (float)c.getCost();
+        if (!titles.isEmpty()) {
+            for (String r : titles) {
+                for (ExpenseObj c : listE) {
+                    if (r.equals(c.getCategory())) {
+                        temp += (float) c.getCost();
                     }
                 }
                 tempList.add(temp);
@@ -64,47 +107,41 @@ public class ExpenseListInterface {
         return tempList;
     }
 
-    public HashMap<String, Float> getCostByDaily(){
+    public HashMap<String, Float> getCostByDaily() {
         HashMap<String, Float> tempList = new HashMap<>();
 
         Calendar cal = Calendar.getInstance();
 
         int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH)+1;
+        int month = cal.get(Calendar.MONTH) + 1;
         int day = cal.get(Calendar.DAY_OF_MONTH);
 
         float temp;
 
 
-        for(int i = 0; i < 30; i++){
+        for (int i = 0; i < 30; i++) {
             temp = 0;
-            for(ExpenseObj c: listE){
-                if(c.getDate().equals(month + "/" + day + "/" + year)){
+            for (ExpenseObj c : listE) {
+                if (c.getDate().equals(month + "/" + day + "/" + year)) {
                     temp += (float) c.getCost();
                 }
             }
 
-            if (day < 10){
-                tempList.put(month + "/0" + day + "/" + year, temp);
-            } else {
-                tempList.put(month + "/" + day + "/" + year, temp);
-            }
-//            tempList.put(month + "/" + day + "/" + year, temp);
+            tempList.put(month + "/" + day + "/" + year, temp);
             System.out.println(month + "/" + day + "/" + year + ": " + temp);
-            if(day != 1){
+            if (day != 1) {
                 day--;
-            }else{
-                if(month!=1){
+            } else {
+                if (month != 1) {
                     month--;
                     day = 31;
-                }else{
+                } else {
                     year--;
                     month = 12;
                     day = 31;
                 }
             }
         }
-        System.out.println("tempList" + tempList);
         return tempList;
     }
 }
