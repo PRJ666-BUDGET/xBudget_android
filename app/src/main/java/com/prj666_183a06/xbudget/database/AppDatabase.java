f4c1cb4507b23c024fc55c84828fd91b3bf9e4f6
jngu36 <jnguyen36@myseneca.ca>
2018-12-03 13:57:10 -0500

    changing from populatedbasync to dbplan

--- app/src/main/java/com/prj666_183a06/xbudget/database/AppDatabase.java
+++ app/src/main/java/com/prj666_183a06/xbudget/database/AppDatabase.java
@@ -48,7 +48,7 @@ public abstract class AppDatabase extends RoomDatabase {
             super.onCreate(db);
             new PopulateDbPlanAsyncTask(instance).execute();
             new PopulateDbExpenseAsyncTask(instance).execute();
-            new PopulateDbAsyncTask(instance).execute();
+            new PopulateDbPlanAsyncTask(instance).execute();
         }
     };
 
